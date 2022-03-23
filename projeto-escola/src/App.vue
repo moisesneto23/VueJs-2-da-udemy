e8401867862170ffbdda97c07b7704c2c2b5b311
Moises das Virgens Cruz Neto <moises.neto@ages.edu.br>
2022-03-23 15:17:30 -0300

    first commit

---
+++ projeto-escola/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div>
+      <Aluno />
+    </div>
+    <div>
+    
+    </div>
+  </div>
+</template>
+
+<script>
+import Aluno from "./components/Aluno.vue";
+export default {
+  name: "App",
+  components: {
+   
+    Aluno,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ projeto-escola/src/components/Aluno.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1> {{qualquercoisa}} </h1>
+        <input type="text" placeholder="Nome do Aluno" v-model="qualquercoisa">
+    </div>
+</template>
+<script>
+export default {
+    data() {//aqui é necessario exportar algo e essa data pode ser qualquer coisa
+        return {
+           qualquercoisa: 'Aluno',
+        }
+    },
+
+}
+</script>
+<style scoped>
+
+</style>
